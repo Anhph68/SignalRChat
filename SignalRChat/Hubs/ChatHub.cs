b9edab27ed69e6f02cabfc86037587f278b0f82f
Kiếm Phong <Anhph68@gmail.com>
2014-03-26 09:02:58 +0700

    Upload files in the project 	new file:   SignalRChat.sln 	new file:   SignalRChat.v12.suo 	new file:   SignalRChat/App_Start/BundleConfig.cs 	new file:   SignalRChat/App_Start/FilterConfig.cs 	new file:   SignalRChat/App_Start/RouteConfig.cs 	new file:   SignalRChat/Content/Site.css 	new file:   SignalRChat/Content/bootstrap.css 	new file:   SignalRChat/Content/bootstrap.min.css 	new file:   SignalRChat/Controllers/HomeController.cs 	new file:   SignalRChat/Global.asax 	new file:   SignalRChat/Global.asax.cs 	new file:   SignalRChat/Hubs/ChatHub.cs 	new file:   SignalRChat/Project_Readme.html 	new file:   SignalRChat/Properties/AssemblyInfo.cs 	new file:   SignalRChat/Scripts/_references.js 	new file:   SignalRChat/Scripts/bootstrap.js 	new file:   SignalRChat/Scripts/bootstrap.min.js 	new file:   SignalRChat/Scripts/jquery-1.10.2.intellisense.js 	new file:   SignalRChat/Scripts/jquery-1.10.2.js 	new file:   SignalRChat/Scripts/jquery-1.10.2.min.js 	new file:   SignalRChat/Scripts/jquery-1.10.2.min.map 	new file:   SignalRChat/Scripts/jquery.signalR-2.0.2.js 	new file:   SignalRChat/Scripts/jquery.signalR-2.0.2.min.js 	new file:   SignalRChat/Scripts/modernizr-2.6.2.js 	new file:   SignalRChat/Scripts/respond.js 	new file:   SignalRChat/Scripts/respond.min.js 	new file:   SignalRChat/SignalRChat.csproj

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMsg(string name, string msg)
+        {
+            Clients.All.SendAll(name, msg);
+        }
+    }
+}
